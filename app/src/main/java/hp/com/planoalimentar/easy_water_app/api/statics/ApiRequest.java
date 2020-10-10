42595c1eeb52ad90c1d96f02c892267fd0072e39
humberto83 <humbertonoa83@gmail.com>
2020-10-10 04:22:50 +0200

    Class to make Request

---
+++ app/src/main/java/hp/com/planoalimentar/easy_water_app/api/statics/ApiRequest.java
@@ -0,0 +1,70 @@
+package hp.com.planoalimentar.easy_water_app.api.statics;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.error.AuthFailureError;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This is a product created by AEISUTC Team on
+ * Matyanga Project concurs
+ * Created by humbertonoa83@gmail.com on 10/10/2020.
+ */
+public class ApiRequest {
+
+    public static void makeRequest (final Context context, final String url, JSONObject jsonObject,
+                                 final CallBack callback){
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, jsonObject, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        if(callback!=null) {
+                            callback.responce(response.toString());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if(callback!=null) {
+                    callback.responce(error.toString());
+                }
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public Map <String, String> getHeaders() throws AuthFailureError {
+                HashMap <String, String> headers = new HashMap<String, String>();
+
+                return headers;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        jsonObjReq.setRetryPolicy(new DefaultRetryPolicy(60000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.getCache().clear();
+        requestQueue.add(jsonObjReq);
+    }
+
+}
---
+++ app/src/main/java/hp/com/planoalimentar/easy_water_app/api/statics/CallBack.java
@@ -0,0 +1,11 @@
+package hp.com.planoalimentar.easy_water_app.api.statics;
+
+/**
+ * This is a product created by AEISUTC Team on
+ * Matyanga Project concurs
+ * Created by humbertonoa83@gmail.com on 10/10/2020.
+ */
+public interface CallBack {
+
+    void responce(String responce);
+}
