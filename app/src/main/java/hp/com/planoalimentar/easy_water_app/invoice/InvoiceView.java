fcfc1f67000dd3ac4c063d42afebf8432edec899
Mabulambe <cheltonmabulambe@gmail.com>
2020-10-17 17:54:53 +0200

    Tela Factura

---
+++ app/src/main/java/hp/com/planoalimentar/easy_water_app/invoice/InvoiceView.java
@@ -0,0 +1,37 @@
+package hp.com.planoalimentar.easy_water_app.invoice;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import hp.com.planoalimentar.easy_water_app.R;
+import hp.com.planoalimentar.easy_water_app.profile.ClientContactFragment;
+import hp.com.planoalimentar.easy_water_app.profile.ClientInformationFragment;
+
+public class InvoiceView extends Fragment {
+
+    private FragmentTransaction fragmentTransaction;
+    private Button button_submit;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_invoice_view, container, false);
+
+        button_submit = view.findViewById(R.id.btn_billing_submit);
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.framelayout, new InvoicePayment());
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
+    }
+}
